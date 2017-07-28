519d672794847381fda9de182ab45885af8dee75
devisreenivas <sreenivas1.reddy@gmail.com>
2017-07-28 09:10:58 +0530

    mavenproject

---
+++ maven-first/src/main/java/www/firstmaven/app/App.java
@@ -0,0 +1,13 @@
+package www.firstmaven.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
